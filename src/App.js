c2e1ddcd54bc1bd4c5fb8c7df680c51d01b353a0
Devakumar MJ <devakumar.mj@gmail.com>
2023-07-10 23:19:32 +0530

    List and onclick functionality completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [movieDetails, setMovieDetails] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://movie-api-pied-six.vercel.app/api/movies') // Replace with your movie API endpoint
+      .then(response => {
+        setMovies(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  const onClickMovie = useCallback((orderDetails) => {
+    axios.get(`https://movie-api-pied-six.vercel.app/api/movies/${orderDetails.id}`) // Replace with your movie API endpoint
+      .then(response => {
+        setMovieDetails(response.data)
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setMovieDetails(null)}>Reset</button>
+      <div>
+        <h1>
+          {movieDetails && <p>{movieDetails.title}</p>}
+        </h1>
+      </div>      
+      <h2>Movie List</h2>
+      <ul>
+        {movies.map(movie => (
+          <li key={movie.id}>
+            <p onClick={() => onClickMovie(movie)}>{movie.title}</p>
+          </li>
+        ))}
+      </ul>
+
     </div>
   );
 }
